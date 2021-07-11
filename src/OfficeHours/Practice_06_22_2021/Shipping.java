ae1bd1a76058f41e954e1b1cde08f704bab5fd37
Muzdalifa Miakhel <mmiakhel@gmail.com>
2021-07-11 10:58:41 -0400

    day 60 exceptions

--- src/OfficeHours/Practice_06_22_2021/Shipping.java
+++ src/OfficeHours/Practice_06_22_2021/Shipping.java
@@ -4,6 +4,11 @@ package OfficeHours.Practice_06_22_2021;
 Create an interface Shipping
     - Declare an abstract methods:
         payForShipping(boolean)
+
+        //interface --> another way in java to achieve abstraction
+                    --> it was pure abstraction in the beginning but after java 8 it is not pure anymore (not pure abstraction)
+                    --> any methods made in abstract class is by default abstract so no need to write abstract in method
+                    --> public by default as well, so no need to write public in method
  */
 
 public interface Shipping {
--- src/OfficeHours/Practice_06_22_2021/Target.java
+++ src/OfficeHours/Practice_06_22_2021/Target.java
@@ -3,17 +3,21 @@ package OfficeHours.Practice_06_22_2021;
 /*
 Create a concrete class Target
     - Inherit Shopping and implement all abstract methods
+
+    concrete --> first non-abstract class of the abstract parent class
  */
 
 public class Target extends Shopping{
 
     @Override
     public void buyItem() {
+
         System.out.println("Buying item from Target");
     }
 
     @Override
     public void returnItem() {
+
         System.out.println("Returning item to Target");
     }
 }
